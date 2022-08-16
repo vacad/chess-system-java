f6e5452c60ef332cc5adcd04ecb593b99f160eca
Yago Neves <yagolife1@gmail.com>
2022-08-16 08:52:44 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -41,7 +41,7 @@ public class UI {
 			return new ChessPosition(column, row);
 		}
 		catch (RuntimeException e) {
-			throw new InputMismatchException("Error readisng chess Position. Valid values are from a1 to h8.");
+			throw new InputMismatchException("Error reading chess Position. Valid values are from a1 to h8.");
 		}
 	}
 	
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,7 +47,7 @@ public class Board {
 	
 	public Piece removePiece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board.");
 		}
 		if(piece(position)==null) {
 			return null;
